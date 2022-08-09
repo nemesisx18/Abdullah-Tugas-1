47754b81fd30143181620b6589c56d79124a9880
DESKTOP-TI065J2\a-10 <DESKTOP-TI065J2\a-10>
2022-08-09 09:45:30 +0800

    init commit

---
+++ Assets/Scripts/ZombieController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    public int speed;
+    
+    public void OnMouseDown()
+    {
+        Debug.Log("Test");
+    }
+}
